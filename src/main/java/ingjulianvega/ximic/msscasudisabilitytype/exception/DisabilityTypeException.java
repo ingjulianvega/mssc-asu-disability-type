5851dbe9cd2ae52e14474af7fe7b882a7e566793
Julian Vega <ingjulianvega@gmail.com>
2021-07-11 18:21:56 -0500

    :sparkles: Error messages

--- src/main/java/ingjulianvega/ximic/msscasudisabilitytype/exception/DisabilityTypeException.java
+++ src/main/java/ingjulianvega/ximic/msscasudisabilitytype/exception/DisabilityTypeException.java
@@ -1,15 +1,20 @@
 package ingjulianvega.ximic.msscasudisabilitytype.exception;
 
-import lombok.Getter;
+import lombok.*;
+import org.springframework.http.HttpStatus;
 
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class DisabilityTypeException extends RuntimeException {
 
-    private final String code;
+    private HttpStatus httpStatus;
+    private String apiCode;
+    private String error;
+    private String message;
+    private String solution;
 
-    public DisabilityTypeException(final String code, final String message) {
-        super(message);
-        this.code = code;
-    }
 }
 
