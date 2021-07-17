4fb6878106277ec95aea965bdc1cc6ff76cf2b28
Julian Vega <ingjulianvega@gmail.com>
2021-07-16 21:28:26 -0500

    :sparkles: Error messages

--- src/main/java/ingjulianvega/ximic/msscasudisabilitytype/services/DisabilityTypeServiceImpl.java
+++ src/main/java/ingjulianvega/ximic/msscasudisabilitytype/services/DisabilityTypeServiceImpl.java
@@ -11,6 +11,7 @@ import ingjulianvega.ximic.msscasudisabilitytype.web.model.DisabilityTypeList;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -39,7 +40,14 @@ public class DisabilityTypeServiceImpl implements DisabilityTypeService {
         log.debug("getById()...");
         return disabilityTypeMapper.disabilityTypeEntityToDisabilityTypeDto(
                 disabilityTypeRepository.findById(id)
-                        .orElseThrow(() -> new DisabilityTypeException(ErrorCodeMessages.DISABILITY_TYPE_NOT_FOUND, "")));
+                        .orElseThrow(() -> DisabilityTypeException
+                                .builder()
+                                .httpStatus(HttpStatus.BAD_REQUEST)
+                                .apiCode(ErrorCodeMessages.DISABILITY_TYPE_NOT_FOUND_API_CODE)
+                                .error(ErrorCodeMessages.DISABILITY_TYPE_NOT_FOUND_ERROR)
+                                .message(ErrorCodeMessages.DISABILITY_TYPE_NOT_FOUND_MESSAGE)
+                                .solution(ErrorCodeMessages.DISABILITY_TYPE_NOT_FOUND_SOLUTION)
+                                .build()));
     }
 
     @Override
@@ -58,7 +66,14 @@ public class DisabilityTypeServiceImpl implements DisabilityTypeService {
     public void updateById(UUID id, DisabilityType disabilityType) {
         log.debug("updateById...");
         DisabilityTypeEntity evidenceEntity = disabilityTypeRepository.findById(id)
-                .orElseThrow(() -> new DisabilityTypeException(ErrorCodeMessages.DISABILITY_TYPE_NOT_FOUND, ""));
+                .orElseThrow(() -> DisabilityTypeException
+                        .builder()
+                        .httpStatus(HttpStatus.BAD_REQUEST)
+                        .apiCode(ErrorCodeMessages.DISABILITY_TYPE_NOT_FOUND_API_CODE)
+                        .error(ErrorCodeMessages.DISABILITY_TYPE_NOT_FOUND_ERROR)
+                        .message(ErrorCodeMessages.DISABILITY_TYPE_NOT_FOUND_MESSAGE)
+                        .solution(ErrorCodeMessages.DISABILITY_TYPE_NOT_FOUND_SOLUTION)
+                        .build());
 
         evidenceEntity.setName(disabilityType.getName());
 
