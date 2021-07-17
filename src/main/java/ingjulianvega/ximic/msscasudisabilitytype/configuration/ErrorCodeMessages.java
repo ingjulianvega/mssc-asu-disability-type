f2fda2f3cdbfcf3945f62f7889c51847c8791db2
Julian Vega <ingjulianvega@gmail.com>
2021-07-16 21:42:13 -0500

    :sparkles: Error messages

--- src/main/java/ingjulianvega/ximic/msscasudisabilitytype/configuration/ErrorCodeMessages.java
+++ src/main/java/ingjulianvega/ximic/msscasudisabilitytype/configuration/ErrorCodeMessages.java
@@ -7,7 +7,7 @@ public class  ErrorCodeMessages {
 
     public static final String DISABILITY_TYPE_NOT_FOUND_API_CODE = "401";
     public static final String DISABILITY_TYPE_NOT_FOUND_ERROR = "BODY_PART_NOT_FOUND";
-    public static final String DISABILITY_TYPE_NOT_FOUND_MESSAGE = "No se encontró la concentración";
+    public static final String DISABILITY_TYPE_NOT_FOUND_MESSAGE = "No se encontró el tipo de incapacidad";
     public static final String DISABILITY_TYPE_NOT_FOUND_SOLUTION = "Verifique los datos";
 
     public static final String ARGUMENT_NOT_VALID_API_CODE = "402";
